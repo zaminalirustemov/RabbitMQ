adf56d75581108804bbae3c0abf6761d238ff021
Zaminali <2g17rck@code.eddu.az>
2023-08-04 19:25:00 +0400

    RabbitMQ

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+var connection = connectionFactory.CreateConnection();
+var chanel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(chanel);
+
+consumer.Received += (model, ea) =>
+{
+    var byteMessage = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(byteMessage);
+    Console.WriteLine($"Oxunan mesaj {message}");
+};
+
+chanel.BasicConsume(queue:"hello",autoAck:true,consumer:consumer);
+
+Console.ReadKey();
---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+var connection = connectionFactory.CreateConnection();
+var chanel = connection.CreateModel();
+
+chanel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var message = "Hello RabbitMQ";
+var byteMessage = Encoding.UTF8.GetBytes(message);
+
+chanel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: byteMessage);
+
+Console.WriteLine("Mesaj gonderildi");
